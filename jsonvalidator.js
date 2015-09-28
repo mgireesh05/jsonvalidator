878f73c6ac5cff332998f20767f2e94155a81f84
gireesh <m.gireesh05@gmail.com>
2015-09-28 11:00:02 +0530

    not reading the whole file into memory, but reading it line by line

--- jsonvalidator.js
+++ jsonvalidator.js
@@ -1,28 +1,33 @@
 var request = require('request');
 var fs = require('fs');
+var tempFile = "temp.html";
 
 var extractJSON = function() {
-   fs.readFile("temp.html", function(err, data) {
-      if (err) {
-         console.error(err);
-      } else {
-         var lines = data.toString().split('\n');
-         var jsonObjs = 0;
-         console.info("No. of lines:", lines.length);
-         for (var i = 0; i < lines.length; i++) {
-            if ((-1 != lines[i].indexOf('{')) || (-1 != lines[i].indexOf('['))) {
-               jsonObjs++;
-            }
-         }
-         console.info("No. of potential jsonObjs:", jsonObjs);
+
+   var rl = require('readline').createInterface({
+      input: require('fs').createReadStream(tempFile)
+   });
+
+   var lineNos = [];
+   var lineNumber = 0;
+
+   rl.on('line', function(line) {
+      lineNumber++;
+      if ((-1 != line.indexOf('{')) || (-1 != line.indexOf('['))) {
+         lineNos.push(lineNumber);
       }
+   });
+
+   rl.on('close', function() {
+      console.info("Potential JSON objects at lines: ", lineNos);
+      console.info("Total = ", lineNos.length);
    });
 };
 
 var processUrl = function(url) {
    request.get(url).on('error', function(err) {
       console.error(err);
-   }).pipe(fs.createWriteStream("temp.html").on('finish', function() {
+   }).pipe(fs.createWriteStream(tempFile).on('finish', function() {
       console.info('done');
       extractJSON();
    }));
