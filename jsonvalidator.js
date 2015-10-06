6c4c6cfb03a3243af66b9f2a33aa855af2d4924a
gireesh <m.gireesh05@gmail.com>
2015-10-06 17:43:25 +0530

    moved processing to python and fixed issue with nested json

--- jsonvalidator.js
+++ jsonvalidator.js
@@ -1,78 +1,44 @@
 var request = require('request');
 var fs = require('fs');
-var BufferBuilder = require('buffer-builder');
 var tempFile = "temp.html";
-
-var extractJSON = function() {
-
-   var rl = require('readline').createInterface({
-      input: require('fs').createReadStream(tempFile)
-   });
-
-   var lineNumber = 0;
-   var jsonStart = false;
-   var jsonBuf;
-   var validJsonCount = 0;
-   var invalidJsonCount = 0;
-   var endChar = null;
-
-   rl.on('line', function(line) {
-      lineNumber++;
-      if (!jsonStart) {
-         var startChar1 = line.indexOf('[');
-         var startChar2 = line.indexOf('{');
-         if ((startChar1 >= 0) || (startChar2 >= 0)) {
-            if (startChar1 >= 0) {
-               endChar = ']';
-            } else if (startChar2 >= 0) {
-               endChar = '}';
-            } else {
-               endChar = ((endChar1 < endChar2) ? ']' : '}');
-            }
-         }
-         if (endChar) {
-            jsonStart = true;
-            jsonBuf = new BufferBuilder();
-            console.info("start:", lineNumber);
-         }
-      }
-
-      if (jsonStart) {
-         jsonBuf.appendString(line);
-         if ((line.indexOf(endChar) >= 0)) {
-            jsonStart = false;
-            endChar = null;
-            console.info("end:", lineNumber);
-            var jsonObj = jsonBuf.get().toString();
-            try {
-               var json = JSON.parse(jsonObj);
-               console.info("====Valid Json====");
-               console.info(json);
-               validJsonCount++;
-               console.info("==================");
-            } catch (e) {
-               console.error("Invalid JSON: ", e, lineNumber);
-               invalidJsonCount++;
-            }
-         }
-      }
-   });
-
-   rl.on('close', function() {
-      console.info("Done!, Valid = ", validJsonCount, "invalid = ", invalidJsonCount);
-   });
-};
+var writeFile = "write.html";
+var PythonShell = require('python-shell');
+var tempFile = 'temp.html';
+var chalk = require('chalk');
 
 var processUrl = function(url) {
-   request.get(url).on('error', function(err) {
-      console.error(err);
-   }).pipe(fs.createWriteStream(tempFile).on('finish', function() {
-      console.info('done');
-      extractJSON();
-   }));
+    request.get(url).on('error', function(err) {
+        console.error(err);
+    }).pipe(fs.createWriteStream(tempFile).on('finish', function() {
+        console.info('done');
+        var options = {
+            mode: 'text',
+            scriptPath: './',
+            args: [tempFile]
+        };
+
+        var shell = new PythonShell('proc.py', options);
+        var validJsonCount = 0;
+        var invalidJsonCount = 0;
+        shell.on('message', function(message) {
+            try {
+                JSON.parse(message);
+                validJsonCount++;
+            } catch (e) {
+                console.error("==============================");
+                console.error(chalk.red("#Invalid JSON#\n", message));
+                console.error("==============================");
+                invalidJsonCount++;
+            }
+        });
+
+        shell.on('close', function() {
+            console.info("Total:: ", chalk.green("Valid: ", validJsonCount), chalk.red(" Invalid: ", invalidJsonCount));
+        });
+    }));
 };
+
 
 var url = "https://github.com/docker/docker/blob/master/docs/reference/api/docker_remote_api_v1.0.md";
 
 processUrl(url);
-//extractJSON();
---
+++ proc.py
@@ -0,0 +1,95 @@
+
+#!/usr/bin/python
+
+import sys
+import json
+import re
+
+
+def main(argv):
+    if len(argv) < 2:
+        sys.exit('Usage: proc.py <inputfile>')
+    preprocess(argv[1])
+    extractJson()
+
+
+def formatLine(line, lineSplitArr):
+    regex = r"[\[\]\{\}]+"
+    match = re.findall(regex, line)
+
+    if match:
+        firstIndex = line.index(match[0])
+        lineSplitArr.append(line[0: firstIndex])
+        lineSplitArr.append(line[firstIndex: firstIndex + 1])
+        line = line[firstIndex + 1: len(line)]
+        formatLine(line, lineSplitArr)
+    else:
+        lineSplitArr.append(line)
+
+
+def preprocess(filename):
+    f = open(filename, 'r')
+    w = open('preprocessed.html', 'w')
+    lineNumber = 0
+    for line in f:
+        lineNumber += 1
+        if ('[' in line) or ('{' in line) or \
+                (']' in line) or ('}' in line):
+            lineSplitArr = []
+            formatLine(line, lineSplitArr)
+            for splitline in lineSplitArr:
+                w.write(splitline)
+                w.write('\n')
+        else:
+            w.write(line)
+    f.close()
+
+
+def extractJson():
+    f = open('preprocessed.html', 'r')
+    lineNumber = 0
+    jsonStart = []
+    jsonObj = []
+    jsonObjs = []
+    jsonStartChar = ""
+    jsonEndChar = ""
+
+    for line in f:
+        lineNumber += 1
+
+        if jsonStartChar:
+            if jsonStartChar in line:
+                jsonStart.append(jsonStartChar)
+            elif jsonEndChar in line:
+                if jsonStart:
+                    jsonStart.pop()
+                else:
+                    jsonObj.append(line.rstrip())
+                    jsonObjs.append(jsonObj)
+                    jsonObj = []
+                    jsonStartChar = ""
+                    jsonEndChar = ""
+            else:
+                jsonObj.append(line.rstrip())
+        else:
+            if '[' in line:
+                jsonStartChar = '['
+                jsonEndChar = ']'
+                jsonObj.append(line.rstrip())
+            elif '{' in line:
+                jsonStartChar = '{'
+                jsonEndChar = '}'
+                jsonObj.append(line.rstrip())
+
+    if jsonStartChar:
+        jsonObjs.append(jsonObj)
+
+    for jobj in jsonObjs:
+        jsonStr = ''
+        for j in jobj:
+            jsonStr += j
+        print jsonStr
+
+    f.close()
+
+main(sys.argv)
